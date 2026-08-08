c1b9fea9f7461ab8f1a3e9391101e1cd8bf3c54b
manepough <manepough@gmail.com>
2026-08-08 08:02:56 +0800

    Add files via upload

--- app/src/main/cpp/client/hooks/egl_hook.cpp
+++ app/src/main/cpp/client/hooks/egl_hook.cpp
@@ -1,1 +1,5 @@
-
+// egl_hook.cpp
+// Plug your eglSwapBuffers hook in here once you have offsets.
+// See the ARM64 hook engine from the earlier files for the pattern.
+#include <android/log.h>
+#define TAG "MCBEClient"
